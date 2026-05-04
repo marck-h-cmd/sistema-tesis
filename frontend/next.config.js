76fbd15eec10f283c1a458aef2751809b42293bb
marck-h-cmd <mahermenegildopa@unitru.edu.pe>
2026-05-04 01:23:11 -0300

    remove manual proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'api.unt.edu.pe'],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/api/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:4000'}/api/:path*`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
