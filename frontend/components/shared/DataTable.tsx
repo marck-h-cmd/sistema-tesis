ce67d94bad8d2955d0b74c0ea6bbd7a9d772dfd9
marck-h-cmd <mahermenegildopa@unitru.edu.pe>
2026-05-03 21:44:41 -0300

    fix DataTable build error in deploy

--- frontend/components/shared/DataTable.tsx
+++ frontend/components/shared/DataTable.tsx
@@ -8,7 +8,8 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table';
+} from '../ui/table';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { ChevronLeft, ChevronRight, Search } from 'lucide-react';
